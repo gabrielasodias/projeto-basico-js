0a78e89edba70520e8560f3ad1b2b910c3eef58a
Gabriela <gabrieladiasofc@gmail.com>
2025-05-03 16:37:58 -0300

    Código completo

--- script.js
+++ script.js
@@ -5,12 +5,34 @@ botao.addEventListener("click", () => {
     const idade = document.querySelector("#idade").value;
     const resposta = document.querySelector("#resposta");
 
-    resposta.style.color = "green";
-
-
-    if(idade >= 18) {
-        resposta.innerHTML = `Olá ${nome}, você é maior de idade!`;
-    } else {
-        resposta.innerHTML = `Olá ${nome}, você ainda é menor de idade!`;
+    if (nome === "" || isNaN(idade) || idade < 0) {
+        resposta.textContent = "Por favor, preencha os campos corretamente!";
+        return;
     }
+    
+    let categoria = "";
+
+    if (idade < 12) {
+    categoria = "uma criança 👶";
+    } else if (idade < 18) {
+    categoria = "um(a) adolescente 🧒";
+    } else if (idade < 60) {
+    categoria = "um(a) adulto(a) 👩‍🦱";
+    } else {
+    categoria = "um(a) idoso(a) 👵";
+    }
+
+    resposta.textContent = `Olá, ${nome}! Você é ${categoria}`;
+    resposta.classList.add("visivel");
+
 })
+
+const limpar = document.querySelector("#limpar");
+
+limpar.addEventListener("click", function () {
+  document.querySelector("#nome").value = "";
+  document.querySelector("#idade").value = "";
+  resposta.textContent = "";
+  resposta.classList.remove("visivel");
+  document.querySelector("#nome").focus();
+});
